3115e846a6e70e1374fc6b584c6c95fc2c52b12a
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2024-12-04 00:40:47 +0530

    contact added

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-
+const contact= {
+    Mahesh : 2383828291,
+    Riya:37372194628,
+    Punit:8535419626,
+    Sushma:2648361937,
+    Jannat:8764396216
+}
 
 function Search(){
-    
+    const searchInput = document.getElementById("search")
+    const query = searchInput.value.toLowerCase();
+    const mobile = contact[query]
+    const result = document.getElementById("result");
+
+    if(mobile){
+        result.innerText="${query} mobile no is ${mobile}";
+    }
+    else{
+        result.innerText= "Not found"; 
+    }
 }
