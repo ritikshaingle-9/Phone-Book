b07ee18a8bed7dc86ad285547a5d25ff5722955e
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2024-12-04 00:57:04 +0530

    js added

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const contact= {
-    Mahesh : 2383828291,
-    Riya:37372194628,
-    Punit:8535419626,
-    Sushma:2648361937,
-    Jannat:8764396216
+    Mahesh :"2383828291",
+    Riya:"37372194628",
+    Punit:"8535419626",
+    Sushma:"2648361937",
+    Jannat:"8764396216"
 }
 
 function Search(){
     const searchInput = document.getElementById("search")
     const query = searchInput.value.toLowerCase();
-    const mobile = contact[query]
+    const mobile = contact[Object.keys(query)];
     const result = document.getElementById("result");
 
     if(mobile){
-        result.innerText="${query} mobile no is ${mobile}";
+        result.innerText=`${Object.keys(query)} mobile no is ${mobile}`;
     }
     else{
         result.innerText= "Not found"; 
